fc92ed5d660fa643a6827b96192e081a3c28da15
Po-Cheng Yeh <blp100x@hotmail.com>
2023-07-02 16:10:39 +0800

    feat: setup fonts, text sizes & colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,63 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    colors: {
+      gold: "#D5966C",
+      white: "#FFFFFF",
+      "almost-black": "#151515",
+      "dark-grey": "#444444",
+    },
+    fontSize: {
+      "heading-xl": [
+        "6rem",
+        {
+          lineHeight: "5.5rem",
+          fontWeight: "900",
+        },
+      ],
+      "heading-l": [
+        "4.375rem",
+        {
+          lineHeight: "4.375rem",
+          fontWeight: "900",
+        },
+      ],
+      "heading-m": [
+        "3.75rem",
+        {
+          lineHeight: "3.125rem",
+          fontWeight: "900",
+        },
+      ],
+      "heading-s": [
+        "2.25rem",
+        {
+          lineHeight: "2.25rem",
+          fontWeight: "900",
+        },
+      ],
+      "body-m": [
+        "1.375rem",
+        {
+          lineHeight: "2rem",
+          fontWeight: "300",
+        },
+      ],
+      "body-s": [
+        "1.125rem",
+        {
+          lineHeight: "1.75rem",
+          fontWeight: "300",
+        },
+      ],
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Big Shoulders Display", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
